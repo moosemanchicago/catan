1acb6cec1bf6ce8e77a1baff3dbcca97fc41c260
moose <moose@007s-Laptop.attlocal.net>
2015-12-24 19:54:24 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Settlers of Catan | Resource Tracker</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.0.6/material.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+
+<div class="container-fluid">
+
+<div class="available-actions">
+<?php
+	$actions = [	'road' => 'Road',
+					'settlement' => 'Settlement',
+					'city' => 'City',
+					'developmentCard' => 'Development Card'
+				];
+	foreach ($actions as $action => $name) :
+?>
+<!-- Primary-colored flat button -->
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent action <?=$action?>" data-action="<?=$action?>">
+<?=$name?>
+</button>
+
+<?php	endforeach;	?>
+</div><!-- available-actions -->
+
+<div class="resource-group">
+<?php
+	$resources = ['brick','wood','stone','wheat','sheep'];
+	foreach ($resources as $resource) :
+?>
+
+
+<ul class="list-group">
+  <li class="list-group-item resource <?=$resource?>" data-resource="<?=$resource?>">
+    <span class="badge resource-count">--</span>
+    <?=$resource?>
+    <span class="steppers">
+		<!-- FAB button with ripple -->
+		<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect remove-resource">
+			<i class="material-icons">-</i>
+		</button>
+		<!-- FAB button with ripple -->
+		<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect add-resource">
+			<i class="material-icons">+</i>
+		</button>
+		<!-- <button type="button" class="btn btn-danger remove-resource">-</button>
+		<button type="button" class="btn btn-success add-resource">+</button> -->
+    </span>
+  </li>
+</ul>
+ <?php	endforeach;	?>
+</div>
+</div><!-- container-fluid -->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.0.6/material.js"></script>
+<script src="script.js"></script>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,124 @@
+(function () {
+
+	var Player = {
+		'brick' : 0,
+		'wood' : 0,
+		'stone' : 0,
+		'wheat' : 0,
+		'sheep' : 0,
+		'developmentCards' : [],
+		'longestRoad' : false,
+		'largestArmy' : false,
+		'roads' : 0,
+		'settlements' : 0,
+		'cities' : 0,
+		'ports' : [],
+		'victoryPoints' : 0,
+		'leader' : false,
+
+		removeResources : function( action ) {
+			for (var resource in ActionCosts[ action ]){
+				Player[ resource ] -= 1;
+				if ( Player [ resource ] < 0 ) { Player[ resource ] = 0; }
+			}
+
+			updateActions();
+		}
+	};
+
+	var ActionCosts = {
+		'road' : {
+			'brick' : 1,
+			'wood' : 1
+		},
+		'settlement' : {
+			'brick' : 1,
+			'wood' : 1,
+			'wheat' : 1,
+			'sheep' : 1,
+		},
+		'city' : {
+			'wheat' : 2,
+			'stone' : 3
+		},
+		'developmentCard' : {
+			'wheat' : 1,
+			'sheep' : 1,
+			'stone' : 1
+		}
+	};
+
+	function updateActions () {
+		// Roads
+		if (	Player.brick >= ActionCosts.road.brick &&
+			Player.wood >= ActionCosts.road.wood ) {
+			$('button.action.road').addClass('btn-success').attr('disabled',false);
+		} else {
+			$('button.action.road').removeClass('btn-success').attr('disabled',true);
+		}
+
+		//Settlements
+		if (	Player.brick >= ActionCosts.settlement.brick &&
+			Player.wood >= ActionCosts.settlement.wood &&
+			Player.wheat >= ActionCosts.settlement.wheat &&
+			Player.sheep >= ActionCosts.settlement.sheep ) {
+			$('button.action.settlement').addClass('btn-success').attr('disabled',false);
+		} else {
+			$('button.action.settlement').removeClass('btn-success').attr('disabled',true);
+		}
+
+		//Cities
+		if (	Player.wheat >= ActionCosts.city.wheat &&
+			Player.stone >= ActionCosts.city.stone ) {
+			$('button.action.city').addClass('btn-success').attr('disabled',false);
+		} else {
+			$('button.action.city').removeClass('btn-success').attr('disabled',true);
+		}
+
+		//Development Card
+		if (	Player.wheat >= ActionCosts.developmentCard.wheat &&
+			Player.sheep >= ActionCosts.developmentCard.sheep &&
+			Player.stone >= ActionCosts.developmentCard.stone ) {
+			$('button.action.developmentCard').addClass('btn-success').attr('disabled',false);
+		} else {
+			$('button.action.developmentCard').removeClass('btn-success').attr('disabled',true);
+		}
+
+		//Update the resource counts from Player
+		$('.resource-count').each(function() {
+			var resource = $(this).parent().data('resource');
+			$(this).text( Player[ resource ]);
+		});
+	}
+
+	$('button.action').attr('disabled',true);
+
+	$('.add-resource').click(function(){
+		var resource = $(this).parent().parent('.resource').data('resource');
+		Player[ resource ] += 1;
+		if ( Player[ resource ] > 0 ) {
+			$(this).attr('disabled',false);
+		}
+		console.info("Added 1 " + resource);
+		$(this).parent().siblings('.resource-count').text( Player[ resource ]);
+		updateActions ();
+	});
+
+	$('.remove-resource').click(function(){
+		var resource = $(this).parent().parent('.resource').data('resource');
+		Player[ resource ] -= 1;
+		if ( Player[ resource ] < 0 ) {
+			Player[ resource ] = 0;
+			$(this).attr('disabled',true);
+		}
+		console.info("Removed 1 " + resource);
+		$(this).parent().siblings('.resource-count').text( Player[ resource ]);
+		updateActions ();
+	});
+
+	$('button.action').click(function(){
+		var action = $(this).data('action');
+		Player.removeResources(action);
+	});
+
+} )();
